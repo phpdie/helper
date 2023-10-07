d13970aa95bb76363f1c90ec0ffe4f44f8d26c1b
phpdie <sundaysunsun@vip.qq.com>
2023-10-07 16:20:16 +0800

    update

--- src/Http.php
+++ src/Http.php
@@ -2,15 +2,50 @@
 
 namespace Helper;
 
+use cURLFile;
+
 class Http
 {
+    /** curl请求
+     * @param string $url
+     * @param $param
+     * @param array $headers
+     * @param string $method
+     * @return bool|string
+     */
+    public static function curl(string $url, $param, array $headers = [], string $method = 'GET')
+    {
+        $curl = curl_init();
+        $opt = [
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => $method,
+            CURLOPT_HTTPHEADER => $headers,
+        ];
+
+        if (strtoupper($method) === 'POST') {
+            $opt[CURLOPT_POSTFIELDS] = $param;
+        }
+
+        curl_setopt_array($curl, $opt);
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        return $response;
+    }
+
     /** get请求
      * @param string $url
-     * @param array $param
+     * @param $param
      * @param array $headers
      * @return bool|string
      */
-    public static function get(string $url, array $param = [], array $headers = [])
+    public static function get(string $url, $param, array $headers = [])
     {
         $parse = parse_url($url);
 
@@ -18,91 +53,42 @@ class Http
         $output = array_merge($output, $param);
         $url = sprintf("%s://%s%s?%s", $parse['scheme'], $parse['host'], $parse['path'], http_build_query($param));
 
-        $curl = curl_init();
-        $opt = [
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'GET',
-            CURLOPT_HTTPHEADER => $headers,
-        ];
-        curl_setopt_array($curl, $opt);
-        $response = curl_exec($curl);
-        curl_close($curl);
-
-        return $response;
+        return self::curl($url, $param, $headers);
     }
 
     /** post请求
      * @param string $url
-     * @param array $param
+     * @param $param
      * @param array $headers
      * @return bool|string
      */
-    public static function post(string $url, array $param = [], array $headers = [])
+    public static function post(string $url, $param, array $headers = [])
     {
-        $curl = curl_init();
-        $opt = [
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => http_build_query($param),
-            CURLOPT_HTTPHEADER => $headers,
-        ];
-        curl_setopt_array($curl, $opt);
-        $response = curl_exec($curl);
-        curl_close($curl);
-
-        return $response;
+        $param = http_build_query($param);
+        return self::curl($url, $param, $headers, 'POST');
     }
 
     /** 表单请求
      * @param string $url
-     * @param array $param
+     * @param $param
      * @param array $headers
      * @param array $files
      * @return bool|string
      */
-    public static function form(string $url, array $param = [], array $headers = [], array $files = [])
+    public static function form(string $url, $param, array $headers = [], array $files = [])
     {
         $fields = $param;
 
         foreach ($files as $key => $path) {
             if (is_array($path)) {
                 foreach ($path as $k => $f) {
-                    $fields[$key . '[' . $k . ']'] = new \cURLFile($f);
+                    $fields[$key . '[' . $k . ']'] = new cURLFile($f);
                 }
             } else {
-                $fields[$key] = new \cURLFile($path);
+                $fields[$key] = new cURLFile($path);
             }
         }
 
-        $curl = curl_init();
-        $opt = [
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => $fields,
-            CURLOPT_HTTPHEADER => $headers,
-        ];
-        curl_setopt_array($curl, $opt);
-        $response = curl_exec($curl);
-        curl_close($curl);
-
-        return $response;
+        return self::curl($url, $fields, $headers, 'POST');
     }
 }
